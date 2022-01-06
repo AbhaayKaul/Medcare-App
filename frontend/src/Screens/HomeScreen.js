09e44aa9293a0634447e0c093c52fe8b9ca73cc6
Aman Kumar <iamankumaaar@gmail.com>
2022-01-06 17:00:19 +0530

    commit to master

---
+++ frontend/src/Screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeCarousel from '../components/HomeCarousel'
+
+function HomeScreen() {
+  return (
+    <>
+        <h1>Home Page</h1>
+        {/* <HomeCarousel /> */}
+    </>
+  )
+}
+
+export default HomeScreen
