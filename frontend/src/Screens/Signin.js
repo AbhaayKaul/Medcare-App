fae027528d4e248a3e6949903c9fb9e1487467c5
Abhaay <abhaay@abhaays-MacBook-Air.local>
2022-01-08 20:53:00 +0530

    added animations in signin and signup

--- frontend/src/Screens/Signin.js
+++ frontend/src/Screens/Signin.js
@@ -1,81 +1,123 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React from "react";
+import { useEffect } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Signin() {
-  const navigate = useNavigate()
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  const navigate = useNavigate();
 
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [userType, setUserType] = React.useState('1')
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [userType, setUserType] = React.useState("1");
 
   const getData = async () => {
     try {
       const config = {
         headers: {
-          'Content-type': 'application/json',
+          "Content-type": "application/json",
         },
-      }
+      };
 
       const { data } = await axios.post(
-        '/api/users/login',
+        "/api/users/login",
         { email, password },
         config
-      )
+      );
 
-      console.log(data)
+      console.log(data);
     } catch (error) {
-      console.log(error)
-      navigate('/login')
+      console.log(error);
+      navigate("/login");
     }
-  }
+  };
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    getData()
-  }
+    e.preventDefault();
+    getData();
+  };
 
   return (
-    <Form onSubmit={submitHandler}>
-      <Form.Group className="mb-3">
-        <Form.Label>Sign In As</Form.Label>
-        <Form.Select enabled onChange={(e) => setUserType(e.target.value)}>
-          <option value="1">Patient</option>
-          <option value="2">Doctor</option>
-          <option value="3">Admin</option>
-        </Form.Select>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  )
+    <>
+      <Form onSubmit={submitHandler}>
+        <Row>
+          <Col md="3"></Col>
+          <Col md="6">
+            <Form.Group className="mb-3">
+              <Form.Label data-aos="fade-left">Sign In As</Form.Label>
+              <Form.Select
+                data-aos="fade-right"
+                enabled
+                onChange={(e) => setUserType(e.target.value)}
+                className="rounded-pill"
+              >
+                <option value="1">Patient</option>
+                <option value="2">Doctor</option>
+                <option value="3">Admin</option>
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col md="3"></Col>
+        </Row>
+        <Row>
+          <Col md="3"></Col>
+          <Col md="6">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label data-aos="fade-left">Email address</Form.Label>
+              <Form.Control
+                data-aos="fade-right"
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="rounded-pill"
+              />
+              <Form.Text data-aos="fade-left" className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+          </Col>
+          <Col md="3"></Col>
+        </Row>
+        <Row>
+          <Col md="3"></Col>
+          <Col md="6">
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label data-aos="fade-left">Password</Form.Label>
+              <Form.Control
+                data-aos="fade-right"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="rounded-pill"
+              />
+            </Form.Group>
+          </Col>
+          <Col md="3"></Col>
+        </Row>
+        <Row>
+          <Col md="3"></Col>
+          <Col md="6" className="text-center">
+            <Button
+              data-aos="fade-down"
+              variant="primary"
+              type="submit"
+              className="rounded-pill"
+            >
+              Login
+            </Button>
+          </Col>
+          <Col md="3"></Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
 
-export default Signin
+export default Signin;
