f62073bf14f54fa87c6e0ae0f169d3149cffcc5c
Aman Verma <vaman644@gmail.com>
2022-01-09 18:13:40 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import morgan from 'morgan'
+// import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
@@ -13,15 +13,11 @@ connectDB()
 
 const app = express()
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'))
+// }
 
 app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.send('API is Running....')
-})
 
 app.use('/api/users', userRoutes)
 
